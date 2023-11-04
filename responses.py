65b2611fbce971b96842f5b1bf1a77a51485a53e
Gav1012 <gavin.poley@gmail.com>
2023-11-04 13:07:03 -0700

    fixed typos with ==

--- responses.py
+++ responses.py
@@ -3,13 +3,13 @@ import random
 def get_response(message: str) -> str:
     p_message = message.lower()
 
-    if p_message = 'hello':
+    if p_message == 'hello':
         return 'HIIIII'
 
-    if message = 'roll':
+    if message == 'roll':
         return str(random.randint(1, 6))
 
-    if p_message = '!help':
+    if p_message == '!help':
         return '`This is a help message (needs to be modified).`'
     
     return 'I didn\'t understand what you wrote. If you need some ideas try "!help".'
